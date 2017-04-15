a50ed00b137e8bb9ce776c51ebeb2246b2266613
CKertz <kertzc@live.com>
2017-04-15 13:11:38 -0600

    Ben's changes to feedActivity merged

--- app/src/main/java/com/example/cooper/baseballbuddy/feedActivity.java
+++ app/src/main/java/com/example/cooper/baseballbuddy/feedActivity.java
@@ -39,7 +39,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class feedActivity extends AppCompatActivity {
+/*public class feedActivity extends AppCompatActivity {
     private ListView listView;
     private FirebaseAuth auth;
     private DatabaseReference mDatabase;
@@ -205,4 +205,193 @@ public class feedActivity extends AppCompatActivity {
         mDatabase.addValueEventListener(userListener);
     }
 
+}
+*/
+
+//BEN's ADDITION BELOW.. v
+
+public class feedActivity extends AppCompatActivity {
+    private ListView listView;
+    ArrayList<String> teamsfeed = new ArrayList<>();
+    private FirebaseAuth auth;
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+        listView = (ListView) findViewById(R.id.lvNewsFeed);
+
+        auth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("teams");
+
+
+    }
+
+    public class newsTask extends AsyncTask<String, String, List<NewsModel>> {
+        @Override
+        protected List<NewsModel> doInBackground(String... params) {
+            BufferedReader reader = null;
+            HttpURLConnection connect = null;
+            HttpURLConnection huc = null;
+
+
+            try {
+                URL url = new URL(params[0]);
+                huc = (HttpURLConnection) url.openConnection();
+                huc.setRequestProperty("Ocp-Apim-Subscription-Key", "01fb8861e5e8434891609c4f6277a72b");
+                huc.connect();
+                InputStream stream1 = huc.getInputStream();
+
+
+                reader = new BufferedReader(new InputStreamReader(stream1));
+
+
+                reader = new BufferedReader(new InputStreamReader(stream1));
+                StringBuffer buffer1 = new StringBuffer();
+
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer1.append(line);
+                }
+
+                String finalJson = buffer1.toString();
+                connection newConnection = new connection();
+                newConnection.getJSONCallAsString(params[0]);
+                List<NewsModel> newsModelList = new ArrayList<>();
+                JSONArray jsonArray = new JSONArray(finalJson);
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    JSONObject finalObject = jsonArray.getJSONObject(i);
+                    NewsModel newsModel = new NewsModel();
+
+                    newsModel.setNewsID(finalObject.getInt("NewsID"));
+                    newsModel.setTitle(finalObject.getString("Title"));
+                    newsModel.setUpdated(finalObject.getString("Updated"));
+                    newsModel.setUrl(finalObject.getString("Url"));
+                    newsModel.setContent(finalObject.getString("Content"));
+                    newsModel.setSource(finalObject.getString("Source"));
+                    newsModel.setTermsOfUse(finalObject.getString("TermsOfUse"));
+                    newsModel.setPlayerID(finalObject.getInt("PlayerID"));
+                    newsModel.setTeamID(finalObject.getInt("TeamID"));
+                    newsModel.setTeam(finalObject.getString("Team"));
+                    int tempID = finalObject.getInt("TeamID");
+                    for (String j : teamsfeed) {
+                        if (j == Integer.toString(tempID)) {
+                            newsModelList.add(newsModel);
+                        }
+
+
+                    }
+                }
+                return newsModelList;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (huc != null) {
+                    huc.disconnect();
+                }
+
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(List<NewsModel> result) {
+            super.onPostExecute(result);
+            NewsAdapter adapter = new NewsAdapter(getApplicationContext(), R.layout.row, result);
+            listView.setAdapter(adapter);
+            // textView.setText(result);
+        }
+    }
+
+    public class NewsAdapter extends ArrayAdapter {
+        private List<NewsModel> newsModelList;
+        private int resource;
+        private LayoutInflater inflater;
+
+        public NewsAdapter(Context context, int resource, List<NewsModel> objects) {
+            super(context, resource, objects);
+            newsModelList = objects;
+            this.resource = resource;
+            inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                convertView = inflater.inflate(resource, null);
+            }
+            TextView Headline;
+            TextView Content;
+
+            Content = (TextView) convertView.findViewById(R.id.tvContent);
+            Headline = (TextView) convertView.findViewById(R.id.tvHeadline);
+
+            //if teamid matches teamID here
+            Headline.setText(newsModelList.get(position).getTitle());
+            Content.setText(newsModelList.get(position).getContent());
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(newsModelList.get(position).getUrl()));
+                    startActivity(intent);
+                }
+            });
+            return convertView;
+        }
+    }
+
+    @Override
+    public void onStart() {
+
+        super.onStart();
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                HashMap<Integer, String> retrievedTeams = new HashMap<Integer, String>();
+
+                dataSnapshot.child("teams").child(auth.getCurrentUser().getUid());
+                ArrayList<userInformation> goals = new ArrayList<userInformation>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    userInformation userTeam = snapshot.getValue(userInformation.class);
+                    Integer id = userTeam.teamID;
+                    String name = userTeam.teamName;
+                    //Toast.makeText(getApplicationContext(), Integer.toString(id), Toast.LENGTH_SHORT).show();
+                    if (id != 0) {
+                        teamsfeed.add(id.toString());
+                    }
+
+                    retrievedTeams.put(id, name);
+                    goals.add(userTeam);
+                }
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabase.addValueEventListener(userListener);
+        new newsTask().execute("https://api.fantasydata.net/mlb/v2/JSON/News");
+    }
+
+    //BELOW IS PASTED CODE FROM SCOREACTIVITY.
 }
